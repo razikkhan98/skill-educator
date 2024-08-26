e06f4ffa5a0ce35136d9a35d04b069e34fc844df
ShumailaKhan12 <shumaila1641@gmail.com>
2024-08-26 17:27:49 +0530

    Changes in navbar

--- src/component/common/navbar/index.jsx
+++ src/component/common/navbar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -13,6 +13,32 @@ const NavigationBar = () => {
     }
   };
   window.addEventListener("scroll", changeColor);
+
+  
+  const [activeLink, setActiveLink] = useState("");
+  const handleScroll = () => {
+    const sections = document.querySelectorAll("section");
+    let currentSection = "";
+
+    sections.forEach((section) => {
+      const sectionTop = section.offsetTop;
+      if (window.scrollY >= sectionTop - 60) {
+        currentSection = section.getAttribute("id");
+      }
+    });
+
+    setActiveLink(currentSection);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeColor);
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", changeColor);
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
@@ -30,16 +56,36 @@ const NavigationBar = () => {
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto d-flex align-items-center ">
-              <Nav.Link className="me-4 fw-bold" href="#Home">
+              <Nav.Link
+                className={`me-4 fw-bold ${
+                  activeLink === "Home" ? "active" : ""
+                }`}
+                href="#Home"
+              >
                 Home
               </Nav.Link>
-              <Nav.Link className="me-4 fw-bold" href="#About">
+              <Nav.Link
+                className={`me-4 fw-bold ${
+                  activeLink === "About" ? "active" : ""
+                }`}
+                href="#About"
+              >
                 About
               </Nav.Link>
-              <Nav.Link className="me-4 fw-bold" href="#Service">
+              <Nav.Link
+                className={`me-4 fw-bold ${
+                  activeLink === "Service" ? "active" : ""
+                }`}
+                href="#Service"
+              >
                 Service
               </Nav.Link>
-              <Nav.Link className="me-4 fw-bold" href="#contact">
+              <Nav.Link
+                className={`me-4 fw-bold ${
+                  activeLink === "Contact" ? "active" : ""
+                }`}
+                href="#Contact"
+              >
                 Contact
               </Nav.Link>
               <Nav.Link className="btn-frame text-center" href="#contact">
