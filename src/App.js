0d5c262ce99bc284852672f61f90e6761c9c5985
ShumailaKhan12 <shumaila1641@gmail.com>
2024-09-13 16:15:30 +0530

    Working in Menu and Loader Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import "./App.css";
 import Home from "./component/pages/home";
+import WhatWeDo from "./component/pages/what-we-do";
 
 function App() {
   return (
     <>
       <Home />
+      {/* <WhatWeDo/> */}
     </>
   );
 }
--- src/component/common/header/index.jsx
+++ src/component/common/header/index.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-
+import { Container } from "react-bootstrap";
 import Logo from "../../assets/img/logo/2.png";
 const Header = ({ title }) => {
   return (
     <>
-      <div className="d-flex align-items-center justify-content-center background-color-light-brown">
-        <div className="header-logo">
-          <img src={Logo} alt="Loading" />
-        </div>
-        <div className="text-center h-25 w-50 background-color-cream">
-          <h2>{title}</h2>
-          <div className="header-bottom-line background-color-brown"></div>
-        </div>
-      </div>
+      <Container fluid className="d-flex align-items-center justify-content-center py-1 text-center background-color-light-brown">
+          <div className="header-logo">
+            <img src={Logo} alt="Loading" />
+            </div>
+          <div className="background-color-cream">
+            <h2 className="px-5 text-lowercase text-center d-inline-flex bd-highlight">{title}</h2>
+            <div className="header-bottom-line background-color-brown"></div>
+          
+          </div>
+
+      </Container>
     </>
   );
 };
