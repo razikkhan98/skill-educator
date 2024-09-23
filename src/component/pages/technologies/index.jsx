d6bbcfa22e9879b25daaf5c76842b01081abdbca
ShumailaKhan12 <shumaila1641@gmail.com>
2024-09-23 19:12:05 +0530

    Changes in Home, footer Page

---
+++ src/component/pages/technologies/index.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+const Technologies = () => {
+  return (
+    <>
+      <div className="gradient-bg py-5">
+        <Container>
+          <Row>
+            <Col md={3}>
+              <div class="flexcard flexcardGreen">
+                <div class="flexcardNumber flexcardNumberGreen">01</div>
+                <div class="flexx flexcardTitle">Title</div>
+                <div class="flexx flexcardText"> </div>
+                <div class="flexx flexcardImg">
+                  <img
+                    class="flexcardimgItem"
+                    src="https://cdn.pixabay.com/photo/2017/01/10/23/01/seo-1970475_960_720.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </Col>
+
+            <Col md={3}>
+              <div class="flexcard flexcardGreen">
+                <div class="flexcardNumber flexcardNumberGreen">01</div>
+                <div class="flexx flexcardTitle">Title</div>
+                <div class="flexxflexcardText"> </div>
+                <div class="flexx flexcardImg">
+                  <img
+                    class="flexcardimgItem"
+                    src="https://cdn.pixabay.com/photo/2017/01/10/23/01/seo-1970475_960_720.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </Col>
+            <Col md={3}>
+              <div class="flexcard flexcardGreen">
+                <div class="flexcardNumber flexcardNumberGreen">01</div>
+                <div class="flexx flexcardTitle">Title</div>
+                <div class="flexx flexcardText"> </div>
+                <div class="flexx flexcardImg">
+                  <img
+                    class="flexcardimgItem"
+                    src="https://cdn.pixabay.com/photo/2017/01/10/23/01/seo-1970475_960_720.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </Col>
+            <Col md={3}>
+              <div class="flexcard flexcardGreen">
+                <div class="flexcardNumber flexcardNumberGreen">01</div>
+                <div class="flexx flexcardTitle">Title</div>
+                <div class="flexx flexcardText"> </div>
+                <div class="flexx flexcardImg">
+                  <img
+                    class="flexcardimgItem"
+                    src="https://cdn.pixabay.com/photo/2017/01/10/23/01/seo-1970475_960_720.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
+export default Technologies;
