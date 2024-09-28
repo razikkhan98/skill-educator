447e53c76026bb9e9179b89560e50395da6e562d
Areeba-Mujeeb <areebamujeeb309@gmail.com>
2024-09-28 13:30:18 +0530

    adding this file

--- src/component/pages/Main/index.jsx
+++ src/component/pages/Main/index.jsx
@@ -6,30 +6,26 @@ import Footer from "../../common/footer";
 import WhatWeDo from "../what-we-do";
 import Technologies from "../technologies";
 import Header from "../../common/header";
-<<<<<<< HEAD
 import Aboutuscard from "../about";
-=======
 import Courses from "../courses";
->>>>>>> 768288e017722bba87a8ae9babad9b0c918ae212
 
 const Main = () => {
   return (
     <React.Fragment>
+      <>
       <Home />
       <Header />
-<<<<<<< HEAD
       <Aboutuscard/>
       <WhatWeDo />
       <Technologies />
       <Footer />
-     </React.Fragment>
-=======
+     <React.Fragment/>
       <WhatWeDo />
       <Technologies />
       <Courses/>
       <Footer />
+      </>
     </React.Fragment>
->>>>>>> 768288e017722bba87a8ae9babad9b0c918ae212
   );
 };
 
